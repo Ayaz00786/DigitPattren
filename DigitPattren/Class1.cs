7b01d35007c4a1b6098f82bb95368871fb7d03b1
Ayaz <Ayaz@DESKTOP-5QKM9AK>
2022-04-25 12:51:21 +0530

    Add project files.

---
+++ DigitPattren/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DigitPattren
+{
+    internal class Class1
+    {
+
+        public static void pattern1()
+        {
+            for (int i = 1; i <= 4; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ DigitPattren/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DigitPattren
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1.pattern1();
+        }
+    }
+}
